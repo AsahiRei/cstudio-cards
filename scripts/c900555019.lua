0eeb0bad058ecf97cdd8d6ae1324951930b7fb49
Yukine <161711269+AsahiRei@users.noreply.github.com>
2026-03-17 08:38:17 +0800

    Add files via upload

---
+++ scripts/c900555019.lua
@@ -0,0 +1,117 @@
+--DAL - Temporal Selves
+--scripted by AsahiRei
+local s,id=GetID()
+function s.initial_effect(c)
+    --activate
+    local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_DESTROYED)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+    e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+    --special summon
+    local e2=DateALive.SpellTrapSpiritEffectProc(c,{target=s.sptg,operation=s.spop,cost=true})
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_REMOVE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetDescription(aux.Stringid(id,2))
+	c:RegisterEffect(e2)
+end
+s.listed_series={SET_DAL,SET_SPIRIT}
+function s.cfilter(c,e,tp)
+    return c:IsPreviousLocation(LOCATION_MZONE) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND|LOCATION_DECK|LOCATION_GRAVE,0,1,nil,e,tp,c:GetCode())
+end
+function s.spfilter(c,e,tp,code)
+	return c:IsCode(code) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    local g=eg:Filter(s.cfilter,nil,e,tp)
+    if chkc then return true end
+	if chk==0 then return eg:IsExists(s.cfilter,1,nil,e,tp) end
+    Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+    local sg=g:Select(tp,1,1,nil,e,tp)
+    Duel.SetTargetCard(sg)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) then
+        local code=tc:GetCode()
+        local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+        if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
+        local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.spfilter),tp,LOCATION_HAND|LOCATION_DECK|LOCATION_GRAVE,0,nil,e,tp,code)
+        if #g>0 and ft>0 then
+            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+            local sg=g:Select(tp,1,ft,nil)
+            local fid=c:GetFieldID()
+            for tc in aux.Next(sg) do
+                Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)
+                tc:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,0,1,fid)
+                local e1=Effect.CreateEffect(c)
+                e1:SetType(EFFECT_TYPE_SINGLE)
+                e1:SetCode(EFFECT_DISABLE)
+                e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+                tc:RegisterEffect(e1)
+                local e2=Effect.CreateEffect(c)
+                e2:SetType(EFFECT_TYPE_SINGLE)
+                e2:SetCode(EFFECT_DISABLE_EFFECT)
+                e2:SetReset(RESET_EVENT|RESETS_STANDARD)
+                tc:RegisterEffect(e2)
+            end
+            Duel.SpecialSummonComplete()
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+            e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+            e1:SetCode(EVENT_PHASE+PHASE_END)
+            e1:SetCountLimit(1)
+            e1:SetLabel(fid)
+            e1:SetLabelObject(sg)
+            e1:SetCondition(s.tdcon)
+            e1:SetOperation(s.tdop)
+            Duel.RegisterEffect(e1,tp)
+        end
+    end
+end
+function s.tdfilter(c,fid)
+	return c:GetFlagEffectLabel(id)==fid
+end
+function s.tdcon(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetLabelObject()
+	if not g:IsExists(s.tdfilter,1,nil,e:GetLabel()) then
+		g:DeleteGroup()
+		e:Reset()
+		return false
+	else return true end
+end
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetLabelObject()
+	local tg=g:Filter(s.tdfilter,nil,e:GetLabel())
+	Duel.SendtoDeck(tg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+end
+function s.desfilter(c,e,tp)
+	return c:IsSetCard(SET_DAL) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,nil,e,tp,c:GetCode())
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.desfilter(chkc,e,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.desfilter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,s.desfilter,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,tp,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)>0 then
+        local code=tc:GetCode()
+        local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND|LOCATION_DECK,0,nil,e,tp,code)
+        if #g>0 then
+            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+            local sg=g:Select(tp,1,1,nil)
+            Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+        end
+    end
+end
