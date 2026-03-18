197bd1276b5aeb8eed8cd767bdad4293a0973226
Yukine <161711269+AsahiRei@users.noreply.github.com>
2026-03-19 06:35:30 +0800

    Delete c900555033.lua

--- c900555033.lua
+++
@@ -1,9 +0,0 @@
---DAL - Kaguya and Yuzuru Yamai
---scripted by AsahiRei
-local s,id=GetID()
-function s.initial_effect(c)
-    --procedure
-    DateALive.Lv3Procedure(c,id,CODE_BERSERK)
-end
-s.listed_series={SET_DAL,SET_SPIRIT}
-s.listed_names={CODE_SPACEQUAKE,CODE_BERSERK}
