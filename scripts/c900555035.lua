9b4f0793344a22e5991a6743aa02191430f64d6e
Yukine <161711269+AsahiRei@users.noreply.github.com>
2026-03-20 22:25:03 +0800

    Add files via upload

--- scripts/c900555035.lua
+++ scripts/c900555035.lua
@@ -4,8 +4,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
     --inverse procedure
-    DateALive.SpiritEffectProcedure(c,id,{category=CATEGORY_DESTROY,target=s.destg,operation=s.desop},false)
-    DateALive.InverseSpiritProcedure(c,CODE_PRINCESS)
+    DateALive.InverseSpiritProcedure(c,CODE_PRINCESS,id,{category=CATEGORY_DESTROY,target=s.destg,operation=s.desop})
     DateALive.AffectedByEffectOfSpiritComrade(c)
     --cannot activate
 	local e1=Effect.CreateEffect(c)
@@ -17,16 +16,6 @@ function s.initial_effect(c)
 	e1:SetValue(s.aclimit)
 	e1:SetCondition(s.actcon)
 	c:RegisterEffect(e1)
-    --destroy replace
-    local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_DESTROY_REPLACE)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e2:SetRange(LOCATION_MZONE)
-    e2:SetCountLimit(1)
-	e2:SetTarget(s.desreptg)
-	e2:SetOperation(s.desrepop)
-	c:RegisterEffect(e2)
 end
 s.listed_names={CODE_PRINCESS}
 s.listed_series={SET_SPIRIT,SET_DAL,SET_INVERSESPIRIT}
@@ -53,19 +42,4 @@ function s.actcon(e)
 end
 function s.aclimit(e,re,tp)
 	return re:IsHasType(EFFECT_TYPE_ACTIVATE)
-end
-function s.repfilter(c,e)
-	return c:IsFaceup() and c:IsSetCard(SET_DAL) and c:IsDestructable(e)
-end
-function s.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return not c:IsReason(REASON_REPLACE) and c:IsOnField() and c:IsFaceup()
-		and Duel.IsExistingMatchingCard(s.repfilter,tp,LOCATION_MZONE,0,1,c,e) end
-	if Duel.SelectEffectYesNo(tp,c,96) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESREPLACE)
-		local g=Duel.SelectMatchingCard(tp,s.repfilter,tp,LOCATION_MZONE,0,1,1,c,e)
-		e:SetLabelObject(g:GetFirst())
-		g:GetFirst():SetStatus(STATUS_DESTROY_CONFIRMED,true)
-		return true
-	else return false end
 end
